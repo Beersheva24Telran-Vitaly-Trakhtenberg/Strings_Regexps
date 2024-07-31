3708c9126bc11e188d68234584768c41c19a06d4
Vitaly Trakhtenberg <webdev@tca.by>
2024-07-31 08:17:13 +0300

    Added signature for the HW#11

--- src/main/java/telran/regexp/Strings.java
+++ src/main/java/telran/regexp/Strings.java
@@ -2,9 +2,65 @@ package telran.regexp;
 
 public class Strings
 {
+    static final String javaKeyWords[] = { "abstract", "assert", "boolean",
+            "break", "byte", "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else", "enum", "extends", "false",
+            "final", "finally", "float", "for", "goto", "if", "implements",
+            "import", "instanceof", "int", "interface", "long", "native",
+            "new", "null", "package", "private", "protected", "public",
+            "return", "short", "static", "strictfp", "super", "switch",
+            "synchronized", "this", "throw", "throws", "transient", "true",
+            "try", "void", "volatile", "while" };
+
+    /**
+     *
+     * @param checking_variable
+     * @return is the word can be used as a name of variable in Java
+     */
     public static boolean isJavaVariable(String checking_variable)
     {
-        String regexp = "([a-z_$])([a-zA-Z_$\\d]*)";
-        return checking_variable.matches(regexp);
+        return checking_variable.matches(regexpJavaVariable()) && !isRegexpJavaReservedNames(checking_variable);
+    }
+
+    /**
+     *
+     * @param checking_string
+     * @return is reserved keyword(-s) presents (as a full separate word) in the given string
+     */
+    public static boolean isRegexpJavaReservedNames(String checking_string)
+    {
+        return checking_string.matches(regexpJavaReservedNames());
+    }
+
+    /**
+     *
+     * @return regExp
+     */
+    public static String regexpJavaVariable()
+    {
+        return "((?!_$)[a-zA-Z$_][\\w$]*)";
+    }
+
+    /**
+     *
+     * @return regExp
+     */
+    public static String regexpJavaReservedNames()
+    {
+        StringBuilder regexBuilder = new StringBuilder();
+        int length = javaKeyWords.length;
+        for (int i = 0; i < length; i++) {
+            regexBuilder.append("\\b").append(javaKeyWords[i]).append("\\b");
+            if (i < length - 1) {
+                regexBuilder.append("|");
+            }
+        }
+        return ".*\\b(" + regexBuilder.toString() + ")\\b.*";
+    }
+
+    public static boolean isArithmeticExpression(String expr)
+    {
+
+        return false;
     }
 }
--- src/test/java/telran/regexp/StringsTest.java
+++ src/test/java/telran/regexp/StringsTest.java
@@ -3,6 +3,7 @@ package telran.regexp;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static telran.regexp.Strings.*;
 
 
@@ -16,15 +17,21 @@ public class StringsTest
         assertTrue(isJavaVariable("_validVariable2"));
         assertTrue(isJavaVariable("$validVariable3"));
         assertTrue(isJavaVariable("$"));
-        assertTrue(isJavaVariable("_"));
         assertTrue(isJavaVariable("_$"));
-        assertTrue(isJavaVariable("valid_variable"));   // non-letter/non-digit
+        assertTrue(isJavaVariable("valid_variable"));   // underscore is valid
+        assertTrue(isJavaVariable("ValidVariable"));   // starts with UpperCase
 
         // Invalid names
         assertFalse(isJavaVariable("1nvalidVariable"));   // starts with digit
-        assertFalse(isJavaVariable("InvalidVariable"));   // starts with UpperCase
         assertFalse(isJavaVariable("1nvаlidVariable"));   // russian a
+        assertFalse(isJavaVariable("extends"));   // use reserved Java keyword
         assertFalse(isJavaVariable("invalid Variable"));   // space
         assertFalse(isJavaVariable("invalid-variable-4"));   // non-letter/non-digit
+        assertFalse(isJavaVariable("_"));    // but underscore only is invalid
+
+        // +++++++++++++++++++++++++++++++++++++++
+        assertTrue(isRegexpJavaReservedNames("test assert isboolean"));
+        assertTrue(isRegexpJavaReservedNames("test assert is boolean"));
+        assertFalse(isRegexpJavaReservedNames("test_assert isBoolean"));
     }
 }
